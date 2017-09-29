e735781fc65a6211f76a1b197051a69ca391388c
a-hilaly <hilalyamine@gmail.com>
2017-09-29 14:53:26 +0200

    Remove

--- build/lib/globql/_dependencies.py
+++
@@ -1,7 +0,0 @@
-
-
-def __mysql_version():
-    pass
-
-def __mysql_connector_version():
-    pass
--- build/lib/globql/_logs.py
+++
@@ -1,9 +0,0 @@
-
-def load_mysql_logs():
-    return "localhost", "root", "uehMLMRw"
-
-def load_mysql_connector_conf():
-    return True, True
-
-def configure_mysql_logs():
-    pass
