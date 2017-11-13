7085b496b0f0dcbe9979a314cdd6f4eb29172c50
a-hilaly <hilalyamine@gmail.com>
2017-11-13 13:53:04 +0100

    Api service

---
+++ sql_tools/api/service.py
@@ -0,0 +1,27 @@
+import os
+
+
+def SQLtools_API(object):
+
+    def __init__(self):
+        pass
+
+    def command_server(command):
+        """
+        0 : Start
+        1 : Stop
+        2 : Reboot
+        """
+        pass
+
+    @classmethod
+    def RunServer(cls):
+        pass
+
+    @classmethod
+    def StopServer(cls):
+        pass
+
+    @classmethod
+    def RestartServer(cls):
+        pass
